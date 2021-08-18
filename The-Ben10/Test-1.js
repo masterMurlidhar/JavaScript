09e2a78f7ddbda780554fd1dc9d6d49e2e467c89
Captain Skywalker <murlidharsingh1319@gmail.com>
2021-08-18 23:22:50 +0530

    Test

---
+++ The-Ben10/Test-1.js
@@ -0,0 +1,6 @@
+
+clear()
+alert("Please! enter your details to continue...");
+prompt("Enter Your E-Mail...");
+prompt("Enter Your Password...");
+alert("Done, I got your details, Have a nice day!");
